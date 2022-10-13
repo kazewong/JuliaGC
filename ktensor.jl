48f574beeeff15a547f759b519d4253fe0235b82
Kaze Wong <kazewong.physics@gmail.com>
2022-10-13 17:26:40 -0400

    fix contraction

--- ktensor.jl
+++ ktensor.jl
@@ -1,7 +1,11 @@
+import Base: +,*
+using Tullio
+
 struct ktensor
-    data :: Array{Float64} # data of the tensor
+    data :: Array # data of the tensor, [k, ndims]
     order ::Int8 # order of the tensor, k
     parity :: Int8 # parity of the tensor, p
+    dimension :: Int8 # dimension of the space where the tensor lives, d
 end
 
 function +(a::ktensor, b::ktensor)::ktensor
@@ -11,21 +15,44 @@ function +(a::ktensor, b::ktensor)::ktensor
     if a.parity != b.parity
         error("Parities of the tensors are not equal")
     end
-    return ktensor(a.data .+ b.data, a.order, a.parity)
+    return ktensor(a.data .+ b.data, a.order, a.parity, a.dimension)
 end
 
 function *(a::ktensor, b::ktensor)::ktensor
     if a.order == 0 || b.order == 0
-        return ktensor(a.data*b.data, a.order, a.parity)
+        return ktensor(a.data*b.data, a.order, a.parity, a.dimension)
     end
-    return ktensor(a.data .* b.data, a.order, a.parity)
+    return ktensor(a.data .* b.data, a.order, a.parity, a.dimension)
 end
 
 function *(a::ktensor, b::Float64)::ktensor
-    return ktensor(a.data*b, a.order, a.parity)
+    return ktensor(a.data*b, a.order, a.parity, a.dimension)
 end
 
 function norm(a::ktensor)::Float64
     return sqrt(sum(a.data.^2))
 end
 
+# Need to implement group element multiplication
+
+function group_mul(a::ktensor, element)::ktensor
+end
+
+function contration(a::ktensor, axis1::Int, axis2::Int)::ktensor
+    ex = :(a.data[])
+    for i in 1:a.order
+        if i == axis1 || i == axis2
+            ex.args = vcat(ex.args,:i)
+        else
+            ex.args = vcat(ex.args,:(:))
+        end
+    end
+    result = eval(:(@tullio S[i] := $ex))
+    return ktensor(result, a.order-2, a.parity, a.dimension)
+end
+
+function levi_civita_contraction(a::ktensor)::ktensor
+
+end
+
+# TODO(Implement test of the functionality here)
