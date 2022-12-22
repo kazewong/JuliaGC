2e8de19d502404acee232c3be5b0ec03358d7c74
Kaze Wong <kazewong.physics@gmail.com>
2022-12-22 13:54:58 -0500

    Make all operators seem working

--- geometric.jl
+++ geometric.jl
@@ -18,7 +18,7 @@ function make_generator(dims::Int)::Vector{Matrix{Int32}}
     return generators
 end
 
-function make_operators(operators, generators)::Matrix{Int32}
+function make_operators(operators::Vector{Matrix{Int32}}, generators)::Vector{Matrix{Int32}}
     for operator in operators
         for generator in generators
             new_operator = generator * operator
@@ -26,13 +26,16 @@ function make_operators(operators, generators)::Matrix{Int32}
             operators = unique(operators)
         end
     end
+    return operators
 end
 
-function make_all_operators(dims::Int)
-    gene
-end
-
-
-
-struct geoFilter
+function make_all_operators(dims::Int)::Vector{Matrix{Int32}}
+    generators = make_generator(dims)
+    operators = Vector{Matrix{Int32}}([Matrix(I,dims,dims)])
+    len = 0
+    while length(operators) != len
+        len = length(operators)
+        operators = make_operators(operators, generators)
+    end
+    return operators
 end
