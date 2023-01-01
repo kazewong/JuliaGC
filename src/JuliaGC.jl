2fbfb72ddfcce54d039ad153ec30069591191940
Kaze Wong <kazewong.physics@gmail.com>
2022-12-31 23:54:41 -0500

    Group actions test passed

--- src/JuliaGC.jl
+++ src/JuliaGC.jl
@@ -5,7 +5,7 @@ using Combinatorics
 include("geometric.jl")
 include("filters.jl")
 
-export Ktensor, make_all_operators, make_generator, make_operators
+export Ktensor, make_all_operators, make_generator, make_operators, contract
 export Image, make_pixel, image_like, get_index, set_index
 export Filter
 
--- test/test_ktensor.jl
+++ test/test_ktensor.jl
@@ -2,28 +2,35 @@ using JuliaGC
 using Test
 
 function test_group_actions(operators::Vector{Matrix{Int32}})::Bool
-    D = size(operators[1],1)
     for parity in [0,1]
-        kt1 = Ktensor(randn(D), parity=parity)
-        kt2 = Ktensor(randn(D), parity=parity)
-        if !vector_dot_vector(kt1, kt2, operators)
-            print("vector_dot_vector failed, parity = $parity")
+        if !vector_dot_vector(Int8(parity), operators)
+            print("vector_dot_vector failed, parity = $parity \n")
             return false
+        else 
+            print("vector_dot_vector passed, parity = $parity \n")
         end
-        if !tensor_times_tensor(kt1, kt2)
-            print("tensor_times_tensor failed, parity = $parity")
+
+        if !tensor_times_tensor(Int8(parity), operators)
+            print("tensor_times_tensor failed, parity = $parity \n")
             return false
+        else 
+            print("tensor_times_tensor passed, parity = $parity \n")
         end
-        if !vectors_dot_tensor(kt1, kt2)
-            print("vectors_dot_tensor failed, parity = $parity")
+        if !vectors_dot_tensor(Int8(parity), operators)
+            print("vectors_dot_tensor failed, parity = $parity \n")
             return false
+        else 
+            print("vectors_dot_tensor passed, parity = $parity \n")
         end
     end
 
     return true
 end
 
-function vector_dot_vector(a::Ktensor, b::Ktensor, operators::Vector{Matrix{Int32}})::Bool
+function vector_dot_vector(parity::Int8, operators::Vector{Matrix{Int32}})::Bool
+    D = size(operators[1],1)
+    a = Ktensor(randn(D), parity=parity)
+    b = Ktensor(randn(D), parity=parity)
     result = true
     a = [a for i in 1:length(operators)]
     b = [b for i in 1:length(operators)]
@@ -38,12 +45,43 @@ function vector_dot_vector(a::Ktensor, b::Ktensor, operators::Vector{Matrix{Int3
     return result
 end
 
-function tensor_times_tensor(a::Ktensor, b::Ktensor)::Bool
-    return true
+function tensor_times_tensor(parity::Int8, operators::Vector{Matrix{Int32}})::Bool
+    D = size(operators[1],1)
+    a = Ktensor(randn(D,D), parity=parity)
+    b = Ktensor(randn(D,D), parity=parity)
+    result = true
+    a = [a for i in 1:length(operators)]
+    b = [b for i in 1:length(operators)]
+    dots = contract.(contract.((a.*operators).*(b.*operators),2,3),1,2)
+    for i in 2:length(dots)
+        if (!isapprox(dots[1].data, dots[i].data))
+            result = false
+            print("vector_dot_vector failed: $(dots[1]) != $(dots[i])")
+            return result
+        end
+    end
+    return result
 end
 
-function vectors_dot_tensor(a::Ktensor, b::Ktensor)::Bool
-    return true
+function vectors_dot_tensor(parity::Int8, operators::Vector{Matrix{Int32}})::Bool
+    D = size(operators[1],1)
+    a = Ktensor(randn(D), parity=parity)
+    b = Ktensor(randn(D), parity=0)
+    c = Ktensor(randn(D,D), parity=parity)
+    result = true
+    a = [a for i in 1:length(operators)]
+    b = [b for i in 1:length(operators)]
+    c = [c for i in 1:length(operators)]
+    dots = contract.(contract.((a.*operators).*(c.*operators).*(b.*operators),2,3),1,2)
+    for i in 2:length(dots)
+        if (!isapprox(dots[1].data, dots[i].data))
+            result = false
+            print("vector_dot_vector failed: $(dots[1]) != $(dots[i])")
+            return result
+        end
+    end
+    return result
 end
 
 @test test_group_actions(make_all_operators(2))
+@test test_group_actions(make_all_operators(3))
